924dd21931ae513e6921512af1996bd23f5c5ff9
Ishan Jindal <86649457+IshanJ25@users.noreply.github.com>
2023-06-20 17:10:24 +0530

    Update example.py

--- example.py
+++ example.py
@@ -3,6 +3,6 @@ import whatsapp_sticker_maker
 # Requirements: you must have these libraries installed with your python package
 # os, pathlib, glob, numpy, PIL, cv2
 
-whatsapp_sticker_maker.make_stickers(folder='doge', output_folder='exports', empty_if_contents=True, animated=False)
+whatsapp_sticker_maker.make_stickers(folder='others\\doge', output_folder='exports')
 
 whatsapp_sticker_maker.give_code_format(folder='exports')
